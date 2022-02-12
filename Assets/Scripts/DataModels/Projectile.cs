63f44284657287f5afa04aa93a155246f70840ea
Dev <dev@divethree.com>
2022-02-12 13:49:28 -0800

    Feature: Collectible Item
    - Add prefab for `CollectibleItem`
    - Add abstract `Item`
    - Add placeholder images
    - implement item pickup events

--- Assets/Scripts/DataModels/Projectile.cs
+++ Assets/Scripts/DataModels/Projectile.cs
@@ -1,12 +1,15 @@
-﻿using UnityEngine;
+﻿using System;
+using Shared;
+using UnityEngine;
 
 namespace DataModels
 {
+    /// <summary>
+    /// All projectiles are used to render game objects and are non-collectible items.
+    /// </summary>
+    [Serializable]
     [CreateAssetMenu(fileName = "New Projectile", menuName = "Game/Projectile", order = 0)]
-    public class Projectile : ScriptableObject
+    public class Projectile : Item
     {
-        public Sprite icon;
-        public string description;
-        public float damage;
     }
 }
--- Assets/Scripts/DataModels/RangedWeapon.cs
+++ Assets/Scripts/DataModels/RangedWeapon.cs
@@ -1,11 +1,22 @@
-﻿using UnityEngine;
+﻿using Shared;
+using UnityEngine;
 
 namespace DataModels
 {
+    /// <summary>
+    /// Any weapon that can be used at range. Collectible.
+    /// </summary>
     [CreateAssetMenu(fileName = "New Ranged Weapon", menuName = "Game/Gun", order = 0)]
-    public class RangedWeapon : ScriptableObject
+    public class RangedWeapon : Item
     {
-        public Sprite icon;
-        public string description;
+        public int projectileCount;
+        public int maxProjectiles;
+        public float damage;
+        
+        /** When this item is collected and the player already has it equipped, the ammo is reloaded. */
+        public void Reload()
+        {
+            projectileCount += 12;
+        }
     }
 }
