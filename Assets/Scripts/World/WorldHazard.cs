d70b8ffd07ec996f366bc65dabfa1c281ff31a09
Dev <dev@divethree.com>
2022-02-20 12:44:49 -0800

    Fix hazards to kill character on contact

---
+++ Assets/Scripts/World/WorldHazard.cs
@@ -0,0 +1,22 @@
+﻿using InController.Scripts;
+using UnityEngine;
+
+namespace World
+{
+    public enum DamageType { Spikes }
+    
+    public class WorldHazard : MonoBehaviour
+    {
+        public DamageType damageType;
+
+        private void OnTriggerEnter2D(Collider2D col)
+        {
+            if (col.CompareTag("Player"))
+            {
+                var player = col.GetComponent<CharacterController2D>();
+                Debug.Log("Player has died by " + damageType);
+                // player.Die();
+            }
+        }
+    }
+}
